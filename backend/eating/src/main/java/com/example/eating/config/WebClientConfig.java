1cf75fb29558e3d2a911a149bdfadc98179647bd
kaa08 <kaa08@naver.com>
2025-12-18 19:05:09 +0900

    fastapi baseurl @Value 사용 업데이트

--- backend/eating/src/main/java/com/example/eating/config/WebClientConfig.java
+++ backend/eating/src/main/java/com/example/eating/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.eating.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${fast.base-url}")
+    private String baseUrl;
+
     @Bean
     public WebClient fastApiClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8000") // FastAPI 주소
+                .baseUrl(baseUrl)
                 .build();
     }
 }
